fba72f23095caa69b6a70c7c46b174f606b31952
안성진 <graph@anseongjin-ui-MacBookAir.local>
2022-05-22 22:26:36 +0900

    main test

---
+++ main.test.js
@@ -0,0 +1,26 @@
+const {test, expect} = require("@jest/globals");
+const {spawn} = require("child_process");
+
+test("Insufficient params", ()=>{
+    const main = spawn("node", ["main.js"]);
+    const outputs = [];
+    main.stdout.on("data", (output)=>{
+        outputs.push(output);
+    });
+    main.stdout.on("end", ()=>{
+        const output = outputs.join("").trim();
+        expect(output).toBe("Insufficient parameter");
+    });
+});
+test("Wrong command", () => {
+     const main = spawn("node", ["main.js", "-as", "-ts"]);
+     const outputs = [];
+     main.stdout.on("data", (output) => {
+         outputs.push(output);
+     });
+     main.stdout.on("end", () => {
+         const output = outputs.join("").trim();
+         expect(output).toBe("Wrong command");
+     });
+ });
+   
